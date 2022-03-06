4eeba0fce29f622c55896f38eaf400465549bcbf
OS_Cool_ <95204181+oscool@users.noreply.github.com>
2022-03-06 14:06:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from discord.ext.commands import has_permissions,  CheckFailure, check
 
 client = discord.Client()
 
-client = commands.Bot(command_prefix = 'ocular!') #put your own prefix here
+client = commands.Bot(command_prefix = 'ocular!') 
 
 slash = SlashCommand(client, sync_commands=True)
 
@@ -54,8 +54,5 @@ async def status(ctx, user):
       embed.add_field(name="Error", value=response.status_code, inline=True)
       await ctx.send(embed=embed)
 keep_up()
-client.run(os.getenv("TOKEN")) #get your bot token and create a key named `TOKEN` to the secrets panel then paste your bot token as the value. 
-#to keep your bot from shutting down use https://uptimerobot.com then create a https:// monitor and put the link to the website that appewars when you run this repl in the monitor and it will keep your bot alive by pinging the flask server
-#enjoy!
-
+client.run(os.getenv("TOKEN")) 
 # invite link - https://discord.com/api/oauth2/authorize?client_id=935764556003823667&permissions=2048&scope=bot%20applications.commands
